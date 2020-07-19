5e9d0184b23dbf3f7b7fd6c53b1bc52e11b7c52e
Jim <github@blyzz.com>
2020-07-18 23:59:01 -0700

    Update firstrun.sh

--- opt/dreamtime/firstrun.sh
+++ opt/dreamtime/firstrun.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+# If file /etc/dreamtime/settings.json DOES NOT EXIST
+# Run /opt/dreamtime/setup.sh
+# Else Run dreamtime.sh
+
 [[ ! -e /etc/dreamtime/settings.json ]] && /opt/dreamtime/setup.sh || dreamtime.sh
