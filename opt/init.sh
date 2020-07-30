63d5f007b55f724cd470060bf8bc4a4860cc3b94
Jim <github@blyzz.com>
2020-07-30 04:17:34 -0700

    Update init.sh

--- opt/init.sh
+++ opt/init.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-$ INITIATE THE CLOCK IN THIS FILE
+# Draw picture of the time
+convert -size 800x600 xc:white -font Nimbus-Roman-No9-L-Regular-Italic -pointsize 300 -gravity center -draw "text 0,0 '$(date +%H:%M)' " /home/cl0ck/time.bmp
+
+# Send that picture to the display
+./IT8951 0 0 /home/cl0ck/time.bmp
