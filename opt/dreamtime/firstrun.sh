d953c847457229d2b61e2506e5fd122f9fdd2432
Jim <github@blyzz.com>
2020-08-06 16:09:24 -0700

    Delete firstrun.sh

--- opt/dreamtime/firstrun.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# If file /etc/dreamtime/settings.json DOES NOT EXIST
-# Run /opt/dreamtime/setup.sh
-# Else Run dreamtime.sh
-
-[[ ! -e /etc/dreamtime/settings.json ]] && /opt/dreamtime/setup.sh || dreamtime.sh
