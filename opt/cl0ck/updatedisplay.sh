ec3d5e927611266c8319048519fc1b2f52d5aba4
Jim <github@blyzz.com>
2020-08-06 01:59:29 -0700

    Create updatedisplay.sh

---
+++ opt/cl0ck/updatedisplay.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+
+#SEND IMAGE TO DISPLAY
+#/IT8951/IT8951 0 0 /tmp/display.bmp
+
+# WHICH CLOCK FACE DO YOU FEEL LIKE SEEING TODAY? DIGITAL OR ANALOGUE
+FLAVOUR=$(cat /etc/cl0ck/settings.json | grep flavour | awk -F: '{print $2}')
+
+#LIGHT OR DARK?
+SCHEME=$(cat /etc/cl0ck/settings.json | grep scheme | awk -F: '{print $2}')
+
+# OK, LET'S BUILD THE IMAGE FOR THE NEXT DISPLAY UPDATE
+# FIRST, LET'S FIGURE OUT WHAT THE TIME WILL BE IN A MINUTE
+
+PDH=$(date +%H) #Pre-Date-Hour
+PDM=$(date +%M) #Pre-Date-Minute
+if [[ $PDM -eq 59 ]];
+then
+        M="00"
+        if [[ $PDH -eq 23 ]];
+        then
+                H="00"
+        else
+                H=$(expr $PDH + 1)
+        fi
+else
+        M=$(expr $PDM + 1)
+fi
+
+# NOW TO DO THE HEAVY LIFTING
+# FIRST UP IS THE DIGITAL CL0CK
+function DIGITAL() {
+
+if [[ $SCHEME = "direct" ]];
+then
+    # Simple Vanilla Clock
+    convert -size 800x600 xc:white -font Nimbus-Roman-No9-L-Regular-Italic -pointsize 300 -gravity center -draw "text 0,0 '$H:$M' " /tmp/display.bmp
+else
+    # Inverse Vanilla
+    convert -size 800x600 xc:black -fill white -font Nimbus-Roman-No9-L-Regular-Italic -pointsize 300 -gravity center -draw "text 0,0 '$H:$M' " /tmp/display.bmp
+fi
+
+}
+
+# NEXT IS THE ANALOGUE
+function ANALOGUE() {
+
+MDEG=$(expr 6 \* $M)
+GEDH=$(expr 30 \* $H)
+GEDM=$(echo "scale = 1; 0.5 * $M" | bc)
+HDEG=$(echo "scale = 1; $GEDM + $GEDH" | bc)
+
+if [[ $SCHEME = "direct" ]];
+then
+    convert -size 800x600 xc:white \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 0 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 30 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 60 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 90 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 120 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 150 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 180 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 210 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 240 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 270 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 300 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate 330 line 0,280 0,260" \
+-stroke black -strokewidth 3 -draw "translate 400.5,300.5 rotate $MDEG line 0,60 0,-240" \
+-stroke black -strokewidth 8 -draw "translate 400.5,300.5 rotate $HDEG line 0,40 0,-150" \
+/tmp/display.bmp
+else
+    convert -size 800x600 xc:black \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 0 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 30 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 60 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 90 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 120 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 150 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 180 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 210 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 240 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 270 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 300 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate 330 line 0,280 0,260" \
+-stroke white -strokewidth 3 -draw "translate 400.5,300.5 rotate $MDEG line 0,60 0,-240" \
+-stroke white -strokewidth 8 -draw "translate 400.5,300.5 rotate $HDEG line 0,40 0,-150" \
+/tmp/display.bmp
+fi
+
+}
+
+# WHAT CLOCK FACE ARE WE GOING TO USE?
+if [[ $FLAVOUR = "digital" ]];
+then
+    DIGITAL
+else
+    ANALOGUE
+fi
